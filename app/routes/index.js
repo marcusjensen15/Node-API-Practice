ad5ced3103bbaf45299c98bfacc3708da832a02b
Marcus Jensen <marcusjensen1515@gmail.com>
2020-06-12 11:22:16 -0700

    getting mongodb up and running locally

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@
 
 const noteRoutes = require('./note_routes');
 
+
 module.exports = function(app, db){
   noteRoutes(app,db);
 }
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
 const port = 8000;
+
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
+//we use body parser because node can't process url encoded forms on its own
 app.use(bodyParser.urlencoded({ extended:true }));
 
 require('./app/routes')(app, {});
